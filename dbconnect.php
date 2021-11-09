fcc8af8407ecb8306a7ee4da92946ab9f7133ff5
ken-tarox <ken-taro@hotmail.com>
2021-11-10 06:07:37 +0900

    commit2

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=roomy;host=localhost;charset=utf8', 'root', 'root');
+    $db = new PDO('mysql:dbname=heroku_2163b7933f34649;host=us-cdbr-east-04.cleardb.com;charset=utf8', 'ba134650246312', '76422bc0');
 }catch(PDOException $e){
     print('DB接続エラー：' . $e->getMessage());
 }
