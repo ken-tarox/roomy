39d0ffe69951fa93f708895ea720b805653108b2
ken-tarox <ken-taro@hotmail.com>
2021-11-10 07:22:58 +0900

    commit7

--- admin/logout.php
+++ admin/logout.php
@@ -4,15 +4,15 @@ session_start();
 
 $_SESSION = array();
 if(ini_get('session.use_cookies')){
-    $params = session_get_cookies_params();
+    $params = session_set_cookies_params();
     setcookie($session_name() . '', time() -42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
 
 session_destroy();
 
 setcookie('email', '', time()-3600);
-setcookie('password', '', time()-3600);
 
 header('Location: login.php');
 exit();
 ?>
+
