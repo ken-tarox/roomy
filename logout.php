1f591f3897133461fa5ce5d69957d4a749a8247b
ken-tarox <ken-taro@hotmail.com>
2021-11-10 07:26:46 +0900

    commit8

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_start();
 
 $_SESSION = array();
 if(ini_get('session.use_cookies')){
-    $params = session_set_cookies_params();
+    $params = session_set_cookie_params();
     setcookie($session_name() . '', time() -42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
 
