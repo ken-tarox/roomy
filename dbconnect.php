485f128c80b70ff4e4dbd9d1150949a7df6c7b36
ken-tarox <65398086+ken-tarox@users.noreply.github.com>
2023-04-02 19:53:33 +0900

    Revert "DB設定"

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=heroku_023486de338ccf1;host=us-cdbr-east-06.cleardb.net;charset=utf8', 'b4d5703c222109', '019b4f7a');
+    $db = new PDO('mysql:dbname=roomy;host=localhost;charset=utf8', 'root', 'root');
 }catch(PDOException $e){
     print('DB接続エラー：' . $e->getMessage());
 }
