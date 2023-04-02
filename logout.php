4236146da7a55b40a40ea5d62fbd95e5595a3ee9
ken-tarox <ken-taro@hotmail.com>
2023-04-02 18:36:31 +0900

    fix/logout.php セッション

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start();
 
 $_SESSION = array();
-if(ini_set('session.use_cookies')){
+if(ini_set("session.use_cookies", 1)){
     $params = session_get_cookies_params();
     setcookie($session_name() . '', time() -42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
